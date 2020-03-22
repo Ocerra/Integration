6cea7463953f547f00212b1b8a6750866bf86dc4
Anton Greb <anton@ocerra.co.nz>
2020-03-22 19:00:16 +1300

    Update Powershell export script to cater for Lower case name properties

--- PowerShell/ExportInvoices.ps1
+++ PowerShell/ExportInvoices.ps1
@@ -28,15 +28,27 @@ function Get-ScriptDirectory {
     Split-Path $script:MyInvocation.MyCommand.Path
 }
 
+function GetTimestamp{
+    $timeStampPath = $(Get-ScriptDirectory) + "\ExportTimestamp.txt"
+    $timeStamp = if(!$(Test-Path $timeStampPath)) { $(Get-Date).AddMonths(-3).ToString($isoDate) } Else { (Get-Content $timeStampPath | select -First 1) }
+    $timeStamp = [datetime]$timeStamp
+    Set-Content $timeStampPath $(Get-Date).ToString($isoDate)
+	return $timeStamp;
+}
+
 function ExportFiles {
 
     #Authenticate using creadentials
-    #$cred = Get-Credential
-
-    #authenticate using hardcoded Login/Pwd
-    #we do not reccomend storing open login or password, use encoded values instead.
-    $user = ""
-    $pass = ""
+    #Get-Credential –Credential (Get-Credential) | Export-Clixml "SecureCredentials.xml"
+	#$Credentials = Import-Clixml "SecureCredentials.xml"
+	#$user = $Credentials.UserName
+	#$pass = $Credentials.GetNetworkCredential().Password
+	
+	$isoDate = "yyyy-MM-dd HH:mm:ss"
+	$settingsPath = $(Get-ScriptDirectory) + "\OcerraSettings.txt"
+	$settings = (Get-Content $settingsPath) -Join "`n" | ConvertFrom-Json
+	$user = $settings.username
+    $pass = $settings.password
     $pair = "${user}:${pass}"
     $bytes = [System.Text.Encoding]::ASCII.GetBytes($pair)
     $base64 = [System.Convert]::ToBase64String($bytes)
@@ -45,9 +57,9 @@ function ExportFiles {
 
     $folder = Get-ScriptDirectory
 
-    $date = Get-Date
+    $timestamp = GetTimestamp
     
-    $folderMonth = $folder + "\" + $date.ToString("yyyy-MM") +"\"
+    $folderMonth = $folder + "\" + $timestamp.ToString("yyyy-MM") +"\"
     
     $dest = New-Item -ItemType Directory -Force -Path $folderMonth
 	
@@ -55,35 +67,40 @@ function ExportFiles {
 
     Write-Host $exportMessage
 
+    $odataRequest = 'https://app.ocerra.com/odata/VoucherHeader?' + 
+		'$skip=0&' + 
+		'$top=500&' + 
+		'$orderby=CreatedDate%20desc&' + 
+		'$count=true&' + 
+		'$filter=IsActive%20eq%20true&UpdatedDate%20ge%20' + $timeStamp.ToString("yyyy-MM-ddTHH:mm:ss") + '&' + 
+		'$expand=Vendor,VoucherLines,Workflow($expand=WorkflowState),Document($expand=DocumentType,StoredFile)'
+
     #retrieve invoices using OData qery
-    $allInvoices = Invoke-RestMethod `
-        -Method Get `
-        -Uri 'https://app.ocerra.com/odata/VoucherHeader?$skip=0&$top=3&$orderby=CreatedDate%20desc&$count=true&$filter=IsActive%20eq%20true&$expand=Vendor,VoucherLines,Workflow($expand=WorkflowState),Document($expand=DocumentType,StoredFile($expand=InverseParentStoredFile))' `
-        -Headers $headers
-        #-Credential $cred
+    $allInvoices = Invoke-RestMethod -Method Get -Uri $odataRequest -Headers $headers        
 
     [System.Collections.ArrayList]$invoices = @();
 
     #convert invoices to a custom format
     ForEach($invoice In $allInvoices.value){
-        $fileName = $invoice.Document.StoredFile.OriginalName;
-        $fileId = $invoice.Document.StoredFile.StoredFileId;
+        $invoiceNumber = If($invoice.number) { $invoice.number } Else { 'Unknown' }
+        $fileName = $invoiceNumber + " - " + $invoice.document.storedFile.originalName;
+        $fileId = $invoice.document.storedFile.storedFileId;
         
         [System.Collections.ArrayList]$invoiceLines = @();
-        ForEach($invoiceLine in $invoice.VoucherLines){
+        ForEach($invoiceLine in $invoice.voucherLines){
             $invoiceLineOrder = $invoiceLines.Add(@{
-                "Description" = $invoiceLine.Description;
-                "Net" = $invoiceLine.Net;
+                "Description" = $invoiceLine.description;
+                "Net" = $invoiceLine.net;
             })
         }
         
         $invoiceOrder = $invoices.Add(@{
-            "Number" = $invoice.Number;
-            "Supplier" = $invoice.Vendor.Name;
-            "Total" = $invoice.Gross;
-            "Net" = $invoice.Net;
-            "Tax" = $invoice.Tax;
-            "DueDate" = $invoice.DueDate;
+            "Number" = $invoice.number;
+            "Supplier" = $invoice.vendor.name;
+            "Total" = $invoice.gross;
+            "Net" = $invoice.net;
+            "Tax" = $invoice.tax;
+            "DueDate" = $invoice.dueDate;
             "Lines" = $invoiceLines;
         });
 
@@ -92,12 +109,12 @@ function ExportFiles {
             -Uri $url `
             -OutFile "$($folderMonth)\$($fileName)" `
             -Headers $headers
-            #-Credential $cred
 
         Write-Host "Invoice file was saved $($fileName)"
     }
 
-    $invoices | ConvertTo-Json -Depth 5 | Out-File "$($folderMonth)\Top 10 Invoices.json" -Encoding ASCII
+	$invoicesFileName = "$($folderMonth)\Invoices at " + $timeStamp.ToString("yyyy-MM-dd HHmmss") + ".json"
+    $invoices | ConvertTo-Json -Depth 5 | Out-File $invoicesFileName -Encoding ASCII
 
     Write-Host "Export complete"
 }
